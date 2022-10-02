5cb31c80da4b4d67593decbf80106c515864ef0b
zeker <zeker@MSI>
2022-10-02 06:02:44 +0300

    char, boolean, integer and float data types added.

---
+++ dataTypes/src/dataTypes/Main.java
@@ -0,0 +1,36 @@
+package dataTypes;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		//INTEGER
+		byte sayi1 = 120;
+		//byte 1 byte -128 to 127
+		
+		short sayi2 = 3000;
+		//short 2 bytes -32768 to 32767
+		
+		int sayi3 = 12565656;
+		//int 4 bytes -2147483648 to 2147483647
+		sayi3 = 13;
+		
+		//long 8 bytes -9223372036854775808 to 9223372036854775807
+		
+		//FLOAT
+		
+		double sayi5 = 12.5;
+		//double  8 bytes 
+		
+		//float 4 bytes
+		
+		char karakter = 'A';
+		System.out.println(karakter);
+		//char 2 bytes
+		
+		boolean dogruMu = true;
+		System.out.println(dogruMu);
+		//boolean 1 byte
+	}
+
+}
